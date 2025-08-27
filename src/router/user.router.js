35c66b2343979e11f672f2f0954fcc1d246484aa
Amaral <dmrramaral@gmail.com>
2025-08-26 21:01:55 -0300

    feat: add user router with CRUD operations for managing users

---
+++ src/router/user.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../model/User');
+const controller = require('../controller/user.controller');
+
+
+
+//Rota para criar um usuário
+router.post('/', controller.createUserController(User));
+
+//Rota para adicionar um produto aos favoritos do usuário
+router.post('/:id/favorites', controller.addFavoriteProductController(User));
+
+
+//Routas para buscar todos os usuários
+router.get('/', controller.getAllUsersController(User));
+
+
+// Rota para buscar um usuário por ID
+router.get('/:id', controller.findByIdController(User));
+
+// Rota para atualizar um usuário por ID
+router.put('/:id', controller.updateUserController(User));
+
+// Rota para deletar um usuário por ID
+router.delete('/:id', controller.deleteUserController(User));
+
+// Rota para remover um produto dos favoritos do usuário
+router.delete('/:id/favorites', controller.deleteFavoriteProductController(User));
+
+
+
+module.exports = router;
