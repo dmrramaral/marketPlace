e4ce1aebdc8b69fe9d2194bef4bfbf4e7283d274
Amaral <dmrramaral@gmail.com>
2025-08-26 21:01:25 -0300

    feat: add user and product routers to the API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectToDatabase = require("./src/database/database");
+const userRouter = require('./src/router/user.router');
+const productRouter = require('./src/router/product.router');
 
 const app = express();
 
@@ -8,6 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 connectToDatabase();
+app.use('/api', userRouter);
+app.use('/api', productRouter);
 
 app.get("/", (req, res) => {
   res.send("Bem-vindo ao servidor de market Place da Loja de calcados!");
