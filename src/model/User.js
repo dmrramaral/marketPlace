9a25aa02d3907a86828b6aff573fc26e44745fd8
Amaral <dmrramaral@gmail.com>
2025-08-28 20:52:39 -0300

    fix: add password hashing middleware to user schema

--- src/model/User.js
+++ src/model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -22,4 +23,12 @@ const UserSchema = new mongoose.Schema({
     },
     admin: { type: Boolean, default: false , required: true }
 });
+
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
 module.exports = mongoose.model('User', UserSchema);
