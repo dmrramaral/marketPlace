0044939098b5dcf77b4dda8ce0a6e6926cf8b7ea
Amaral <dmrramaral@gmail.com>
2025-08-28 22:02:03 -0300

    feat: add authentication middleware to handle JWT verification

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+const jwt  = require('jsonwebtoken');
+const findByIdService = require('../service/user.service').findByIdService;
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token não fornecido' });
+    }
+    const token = authHeader.split(" ");
+  
+   
+    if(token.length !== 2){
+        return res.status(401).json({ message: 'Token mal formatado' });
+    }
+    const [scheme, credentials] = token;
+    console.log(scheme, credentials);
+    if(!/^Bearer$/i.test(scheme)){
+        return res.status(401).json({ message: 'Token mal formatado' });
+    }
+
+    try {
+        jwt.verify(credentials, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) {
+                return res.status(500).json({ message: 'Token inválido', error: err.message });
+            }
+            const user = await findByIdService(decoded.id);
+           
+            if (!user) {
+                return res.status(401).json({ message: 'Token inválido' });
+            }
+            req.user = decoded;
+            return next();
+        });
+
+    } catch (error) {
+        return res.status(401).json({ message: 'Token inválido', error: error.message });
+    }
+}
+module.exports = authMiddleware;
