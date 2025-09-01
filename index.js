e00c1cef4f85531fd77a4dcc390e71160a5c5acc
Amaral <dmrramaral@gmail.com>
2025-09-01 20:15:47 -0300

    fix: reorder API routes for category and product management

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const connectToDatabase = require("./src/database/database");
 const userRouter = require('./src/router/user.router');
 const authRouter = require('./src/router/auth.router');
 const productRouter = require('./src/router/product.router');
+const categoryRouter = require('./src/router/category.router');
 
 require('dotenv').config();
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -15,19 +15,15 @@ app.use(express.json());
 connectToDatabase();
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-app.use('/api', productRouter);
+app.use('/api/category', categoryRouter);
+app.use('/api/product', productRouter);
 
 app.get("/", (req, res) => {
   res.send("Bem-vindo ao servidor de market Place da Loja de calcados!");
 });
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
-
-app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
-  connectToDatabase();
 });
 
 
