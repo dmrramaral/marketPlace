0d0f5f6f07a038adb5bee802613cf39a1bd8dc04
Amaral <dmrramaral@gmail.com>
2025-09-01 19:58:53 -0300

    fix: update user router to use consistent userController reference

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/User');
-const controller = require('../controller/user.controller');
+const userController = require('../controller/user.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
 
 
 //Rota para criar um usuário
-router.post('/create', controller.createUserController(User));
+router.post('/create', userController.createUserController(User));
 
 //Rota para adicionar um produto aos favoritos do usuário
-router.post('/:id/favorites', controller.addFavoriteProductController(User));
+router.post('/:id/favorites', userController.addFavoriteProductController(User));
 
 
 //Routas para buscar todos os usuários
-router.get('/', controller.getAllUsersController(User));
+router.get('/', userController.getAllUsersController(User));
 
 
 // Rota para buscar um usuário por ID
-router.get('/:id',authMiddleware, controller.findByIdController(User) );
+router.get('/:id',authMiddleware, userController.findByIdController(User) );
 
 // Rota para atualizar um usuário por ID
-router.put('/:id', controller.updateUserController(User));
+router.put('/:id', userController.updateUserController(User));
 
 // Rota para deletar um usuário por ID
-router.delete('/:id', controller.deleteUserController(User));
+router.delete('/:id', userController.deleteUserController(User));
 
 // Rota para remover um produto dos favoritos do usuário
-router.delete('/:id/favorites', controller.deleteFavoriteProductController(User));
+router.delete('/:id/favorites', userController.deleteFavoriteProductController(User));
 
 
 
